2a8d2506bcfd4b90db63aa54531aa9dda710677b
Heley Chen <zccz14@outlook.com>
2015-11-03 16:01:59 +0800

    #5
    ADD：Wallshall算法求可达矩阵
    NOTE：方法函数句柄赋值以及return JSON后记得分号

--- graph.js
+++ graph.js
@@ -5,13 +5,17 @@ function graph() {
 		Mat: [],
 		List: []
 	}; //数据类
-	this.set = {}; //设置类
+	this.set = {
+		ByList: undefined,
+		ByMat: undefined
+	}; //设置类
 	this.attr = {}; //性质类
 	this.print = {}; //打印类
 	this.is = {}; //谓词类
 	this.has = {}; //存在类
 	this.calc = {
-		Deg: {} //度数
+		Deg: {}, //度数
+		ReachableMatrix: undefined //计算可达矩阵
 	}; //计算类
 	this.set.ByList = function(list) { //由邻接链表创建
 		if (list.length == undefined)
@@ -51,64 +55,63 @@ function graph() {
 			status: true,
 			response: "邻接链表创建成功"
 		};
-	}
+	};
 	this.set.ByMat = function(mat) { //由矩阵创建
-			if (mat.length == undefined)
-				return {
-					status: false,
-					response: "mat非矩阵，创建失败"
-				};
-			if (mat.length == 0)
-				return {
-					status: false,
-					response: "mat为空，创建失败"
-				};
-			var size = mat[0].length;
-			if (size != mat.length)
-				return {
-					status: false,
-					response: "mat长宽不等，创建失败"
-				};
-			for (var i = 0; i < size; i++)
-				if (size != mat[i].length)
-					return {
-						status: false,
-						response: "mat每行不等长，创建失败"
-					};
-				//合法的数据
-			data = {};
-			data.Mat = mat;
-			//同步到邻接链表
-			data.List = [];
-			for (var i = 0; i < size; i++) {
-				data.List.push([]);
-				for (var j = 0; j < size; j++)
-					for (var k = 0, lenk = mat[i][j]; k < lenk; k++)
-						data.List[i].push(j);
-			}
-			attr = {}; //清空性质
-			data.ready = true; //数据准备完毕
+		if (mat.length == undefined)
 			return {
-				status: true,
-				response: "创建成功"
+				status: false,
+				response: "mat非矩阵，创建失败"
 			};
+		if (mat.length == 0)
+			return {
+				status: false,
+				response: "mat为空，创建失败"
+			};
+		var size = mat[0].length;
+		if (size != mat.length)
+			return {
+				status: false,
+				response: "mat长宽不等，创建失败"
+			};
+		for (var i = 0; i < size; i++)
+			if (size != mat[i].length)
+				return {
+					status: false,
+					response: "mat每行不等长，创建失败"
+				};
+			//合法的数据
+		data = {};
+		data.Mat = mat;
+		//同步到邻接链表
+		data.List = [];
+		for (var i = 0; i < size; i++) {
+			data.List.push([]);
+			for (var j = 0; j < size; j++)
+				for (var k = 0, lenk = mat[i][j]; k < lenk; k++)
+					data.List[i].push(j);
 		}
-		//打印输出类
+		attr = {}; //清空性质
+		data.ready = true; //数据准备完毕
+		return {
+			status: true,
+			response: "创建成功"
+		};
+	};
+	//打印输出类
 
-	this.print.Mat = function() { //打印邻接矩阵
+	this.print.Mat = function(Mat) { //打印矩阵
 		if (!data.ready) return {
 			status: false,
 			response: "数据未准备好"
 		};
-		var Mat = data.Mat;
+		//var Mat = data.Mat;
 		for (var i = 0, leni = Mat.length; i < leni; i++)
-		//for(var j=0,lenj=Mat[i].length;j<lenj;j++)
 			console.log(Mat[i]);
 		return {
 			status: true,
-			response: "打印邻接矩阵成功"
+			response: "打印矩阵成功"
 		};
-	}
+	};
 
 	this.is.Undirected = function() { //是无向图吗？
 		if (!data.ready)
@@ -134,7 +137,7 @@ function graph() {
 			status: true,
 			response: "是无向图"
 		};
-	}
+	};
 	this.calc.Deg.Out = function(vertex) { //计算出度
 		if (data.ready) {
 			if (vertex < 0 || vertex >= data.Mat.length) return {
@@ -150,32 +153,59 @@ function graph() {
 			status: false,
 			response: "数据未准备完毕"
 		};
-	}
+	};
 	this.calc.Deg.In = function(vertex) { //计算入度
-			if (data.ready) {
-				if (vertex < 0 || vertex >= data.Mat.length) return {
-					status: false,
-					response: "结点下标越界"
-				}
-				if (attr.Undirected == true) return { 
-					//如果有无向图的性质，优化到O(1)
-					status: true,
-					response: calc.Deg.Out(vertex).response
-				}
-				var sum = 0;
-				//累计各点到vertex的边数和,O(V)
-				for (var i = 0, len = data.Mat.length; i < len; i++)
-					sum += data.Mat[i][vertex];
-				return {
-					status: true,
-					response: sum
-				};
-			} else return {
+		if (data.ready) {
+			if (vertex < 0 || vertex >= data.Mat.length) return {
 				status: false,
-				response: "数据未准备完毕"
+				response: "结点下标越界"
 			};
-		}
-		//End
+			if (attr.Undirected == true) return {
+				//如果有无向图的性质，优化到O(1)
+				status: true,
+				response: calc.Deg.Out(vertex).response
+			}
+			var sum = 0;
+			//累计各点到vertex的边数和,O(V)
+			for (var i = 0, len = data.Mat.length; i < len; i++)
+				sum += data.Mat[i][vertex];
+			return {
+				status: true,
+				response: sum
+			};
+		} else return {
+			status: false,
+			response: "数据未准备完毕"
+		};
+	};
+	this.calc.ReachableMatrix = function() {//求可达矩阵
+		if (data.ready) {
+			if (data.ReachableMatrix != undefined) return {
+				status: true,
+				response: "从缓存取得可达矩阵"
+			};
+			//Wallshall算法,O(V^3)
+			var R = data.Mat;
+			var len = R.length;
+			//Boolean化
+			for (var i = 0; i < len; i++)
+				for (var j = 0; j < len; j++)
+					R[i][j] = !!R[i][j];
+			for (var k = 0; k < len; k++)
+				for (var i = 0; i < len; i++)
+					for (var j = 0; j < len; j++)
+						R[i][j] |= (R[i][k] && R[k][j]);
+			data.ReachableMatrix = R;
+			return {
+				status: true,
+				response: "Wallshall算法求可达矩阵成功"
+			};
+		} else return {
+			status: false,
+			reponse: "数据未准备好"
+		};
+	};
+	//End
 	return this;
 };
 //Example Call
@@ -191,13 +221,11 @@ var list = [
 ];
 var log = [];
 log.push(G.set.ByMat(mat));
-log.push(G.print.Mat());
 log.push(G.data.List);
-//log.push(G.set.ByList(list));
-log.push(G.print.Mat());
 log.push(G.calc.Deg.Out(0)); //计算结点0的出度
-log.push(G.data.Mat);
 log.push(G.data.List);
 log.push(G.is.Undirected());
 log.push(G.calc.Deg.In(1)); //计算结点1的入度
+log.push(G.calc.ReachableMatrix()); //求可达矩阵
+log.push(G.print.Mat(G.data.ReachableMatrix)); //打印可达矩阵
 console.log(log);
